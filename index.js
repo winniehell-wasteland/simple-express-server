c5b4edab1492d2377b0130c165b82147a9d1316e
winniehell <git@winniehell.de>
2016-04-02 18:39:56 +0200

    read host name from environment variable or package.json config

--- index.js
+++ index.js
@@ -6,16 +6,9 @@ module.exports = function(app) {
 
   var packageJson = require(path.join(packpath.parent(), 'package.json'));
 
-  var port = parseInt(process.env.PORT) || parseInt(packageJson.config.port);
+  var port = parseInt(process.env.PORT) || parseInt(packageJson.config.port) || 62000;
+  var hostName = process.env.HOSTNAME || packageJson.config.hostname || 'localhost';
 
-  if (port) {
-    console.log('Running ' + packageJson.name + ' at port ' + port + ' ...');
-    app.listen(port);
-  } else {
-    var hostName = packageJson.name + '.node.js';
-    port = 62000;
-    console.log('Running ' + packageJson.name + ' at http://' + hostName + ':' + port + ' ...');
-    app.listen(port, hostName);
-  }
-
+  console.log('Running ' + packageJson.name + ' at http://' + hostName + ':' + port + ' ...');
+  app.listen(port, hostName);
 };
