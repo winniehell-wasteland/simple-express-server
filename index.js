68a009e62a73c942ce5fa34b949db3c4e0facc9b
winniehell <git@winniehell.de>
2016-04-02 18:13:52 +0200

    add package name to log

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ module.exports = function(app) {
   var packpath = require('packpath');
   var path = require('path');
 
+  var config = require(path.join(packpath.parent(), 'package.json'));
+
   var port = parseInt(process.env.PORT);
 
   if (port) {
-    console.log('Running at port ' + port + ' ...');
+    console.log('Running ' + config.name + ' at port ' + port + ' ...');
     app.listen(port);
   } else {
-    var config = require(path.join(packpath.parent(), 'package.json'));
     var hostName = config.name + '.node.js';
     port = 62000;
-    console.log('Running at http://' + hostName + ':' + port + ' ...');
+    console.log('Running ' + config.name + ' at http://' + hostName + ':' + port + ' ...');
     app.listen(port, hostName);
   }
 
