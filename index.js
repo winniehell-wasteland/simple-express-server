fa443260a62c503710fe5d9f6ea655737c869e4e
winniehell <git@winniehell.de>
2016-04-02 19:03:28 +0200

    only use environment variables PORT and HOSTNAME in production environment

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-module.exports = function(app) {
+module.exports = function (app) {
   'use strict';
 
   var packpath = require('packpath');
   var path = require('path');
 
   var packageJson = require(path.join(packpath.parent(), 'package.json'));
-  var port = parseInt(process.env.PORT) || parseInt(process.env.npm_package_config_port) || 3000;
-  var hostName = process.env.HOSTNAME || process.env.npm_package_config_hostname || 'localhost';
+
+  var port, hostName;
+  if (process.env.NODE_ENV === 'production') {
+    port = parseInt(process.env.PORT);
+    if (!port) {
+      throw new Error('There is no fallback port in production environment!');
+    }
+
+    hostName = process.env.HOSTNAME;
+    if (!hostName) {
+      throw new Error('There is no fallback host name in production environment!');
+    }
+  } else {
+    port = parseInt(process.env.PORT) || parseInt(process.env.npm_package_config_port) || 3000;
+    hostName = process.env.HOSTNAME || process.env.npm_package_config_hostname || 'localhost';
+  }
 
   console.log('Running ' + packageJson.name + ' at http://' + hostName + ':' + port + ' ...');
   app.listen(port, hostName);
